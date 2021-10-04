db02d63d4f9c7b9bb6bace693d6875c6bb09b49a
Wessel131 <83341367+Wessel131@users.noreply.github.com>
2021-10-04 17:59:25 +0200

    update

---
+++ js/script.js
@@ -0,0 +1,35 @@
+var pets, age = 
+[
+    {type: "cat",
+    age: 5.5,},
+
+    {type: "dog",
+    age: 3.8,},
+
+    {type: "parrot",
+    age: 4.0,},
+];
+
+let text = "";
+for (let i = 0; i >= pets.age.float; i++) {
+    text += pets[i] + "<br>";
+}
+
+var pets = [
+    for (i = age; i >= 4)
+    {
+      type: "cats",
+      age: 5.5,
+    },
+    {
+      type: "dog",
+      age: 3.8,
+    },
+    {
+      type: "parrot",
+      age: 4.0,
+    },
+      console.log("The", , "is", 4, "years or older");
+  ];
+  var i = pets.
+  var age = pets.valueOf.age
